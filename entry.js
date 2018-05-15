3cb2131dccd56cd065c81a7b647f6033036aa150
Glitch (hmm-how-does-this) <none>
2018-05-15 02:25:10 +0000

    🏯🌜 Updated with Glitch

---
+++ entry.js
@@ -0,0 +1,1 @@
+console.log('webpackin it');
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+console.log(path.resolve(__dirname, 'public'));
+
+// module.exports = {
+//   entry: './entry.js',
+//   output: {
+//     path: path.resolve(__dirname, 'public'),
+//     filename: 'entry.bundle.js'
+//   }
+// }
